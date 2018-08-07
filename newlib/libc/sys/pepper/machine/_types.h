027fdcf91bb0224ccd1b23ffba93fba3d87d9c56
Jason Lu <syscallrax@gmail.com>
2018-08-06 20:48:13 -0500

    pepper: Add types overriding

---
+++ newlib/libc/sys/pepper/machine/_types.h
@@ -0,0 +1,18 @@
+#ifndef _MACHINE__TYPES_H
+#define _MACHINE__TYPES_H
+
+#include <machine/_default_types.h>
+
+typedef __uint64_t __dev_t;
+#define __machine_dev_t_defined
+
+typedef __int64_t _off_t;
+#define __machine_off_t_defined
+
+typedef __uint64_t __ino_t;
+#define __machine_ino_t_defined
+
+typedef __uint32_t __mode_t;
+#define __machine_mode_t_defined
+
+#endif /* _MACHINE__TYPES_H */
